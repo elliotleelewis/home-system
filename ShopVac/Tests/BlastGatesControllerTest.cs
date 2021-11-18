3b347eb2c5501636d0127eaba7f28500c70dbcd6
Elliot Lewis <elliotleelewis@gmail.com>
2021-11-18 09:24:10 -0500

    ci: added pipeline dockerfiles

---
+++ ShopVac/Tests/BlastGatesControllerTest.cs
@@ -0,0 +1,14 @@
+using ShopVac.Controllers;
+using Xunit;
+
+namespace Tests
+{
+	public class BlastGatesControllerTest
+	{
+		[Fact]
+		public void ItExists()
+		{
+			Assert.NotNull(typeof(BlastGatesController).FullName);
+		}
+	}
+}
