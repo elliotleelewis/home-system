a2fa7f4050e5db24b38ce68abed8fc890ddf975b
Elliot Lewis <elliotleelewis@gmail.com>
2023-01-06 21:07:05 -0800

    chore: fixed plugin import

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const glob = require('glob');
-const purgecssWebpackPlugin = require('purgecss-webpack-plugin');
+const { PurgeCSSPlugin } = require('purgecss-webpack-plugin');
 
 const PATHS = {
 	src: path.join(__dirname, 'src'),
@@ -8,7 +8,7 @@ const PATHS = {
 
 module.exports = (cfg) => {
 	cfg.plugins.push(
-		new purgecssWebpackPlugin({
+		new PurgeCSSPlugin({
 			paths: () => glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
 		}),
 	);
