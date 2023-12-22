104b9294a720681446f3893caab910e949856499
Elliot Lewis <elliotleelewis@gmail.com>
2023-12-22 14:13:02 -0800

    chore(deps): update lock file

--- client/setup-jest.ts
+++ client/setup-jest.ts
@@ -1,3 +1,6 @@
+import 'jest-preset-angular/setup-jest';
+import { ngMocks } from 'ng-mocks';
+
 /**
  * Monkey patch console warn and error to fail if a test makes calls to console.warn or console.error.
  */
@@ -13,3 +16,5 @@ console.error = (message?: unknown, ...optionalParams: unknown[]): void => {
 		`Test contained console error:\n${String(message)}${params}`,
 	);
 };
+
+ngMocks.autoSpy('jest');
